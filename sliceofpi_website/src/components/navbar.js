76016c637cf7274b93f67575476cf589426c6787
Daniel Albino <danielalbino@localhost.localdomain>
2020-01-05 11:01:22 +0000

    added hamburger icon from font awsome

--- sliceofpi_website/src/components/navbar.js
+++ sliceofpi_website/src/components/navbar.js
@@ -4,10 +4,10 @@ class Navbar extends Component {
     render(){
         return(
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars fa-2x"></i>
                 </button>
-                <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav ml-auto">
                         <a class="nav-item nav-link" href="#home">Home</a>
                         <a class="nav-item nav-link" href="#work">Work</a>
